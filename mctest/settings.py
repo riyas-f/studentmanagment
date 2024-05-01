06cef97563acef6fb4a6d808ab319adb102b9dbf
Riyas F <150213984+riyasbcompany@users.noreply.github.com>
2024-05-01 23:29:19 +0530

    final

--- mctest/settings.py
+++ mctest/settings.py
@@ -47,23 +47,14 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
-    'http://64.226.81.32',
-    'http://64.226.81.32:1337'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
-    'http://64.226.81.32',
-    'http://64.226.81.32:1337'
 ]
 
 CORS_ORIGINS_WHITELIST = [
     'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
-    'http://64.226.81.32',
-    'http://64.226.81.32:1337'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
